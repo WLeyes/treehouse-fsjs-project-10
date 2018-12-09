86b54c56d6740392e4bb7b0f1b011002d72d8ce8
Warren Leyes <2774431+WLeyes@users.noreply.github.com>
2018-12-09 15:32:05 -0800

    test

--- client/src/Components/Courses/Course.js
+++ client/src/Components/Courses/Course.js
@@ -5,6 +5,7 @@ import Fade from "react-reveal/Fade";
 
 const Course = props => {
   // render tamplate of each individual course card.
+  // 
   return (
     <div className="grid-33">
       <Fade left big cascade>
